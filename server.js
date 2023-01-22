c65552903887c6f6cd603a6f96df152eb0ba1951
Vishal21121 <vs84077000@gmail.com>
2023-01-22 19:02:01 +0530

    added github notification feature

--- server.js
+++ server.js
@@ -25,6 +25,19 @@ client.on('ready',()=>{
     
 })
 
+let userId = {
+}
+
+const getCount = async(author)=>{
+    let time = new Date()
+    let actualTime = `${time.getFullYear()}-${time.getMonth().toString().length<2?"0"+parseInt(time.getMonth()+1):time.getMonth()+1}-${time.getDate()}`
+    let response = await fetch(`https://api.github.com/search/commits?q=author:${author}+committer-date:${actualTime}`,{
+        method:'GET'
+    })
+    let data = await response.json()
+    return data
+}
+
 client.on("messageCreate",async(message)=>{
     let data;
     if (message.content.startsWith("/chat-gpt")){
@@ -39,7 +52,20 @@ client.on("messageCreate",async(message)=>{
             presence_penalty: 0,
         });
         message.reply(response.data.choices[0].text)
-        
     }
-    
+    else if (message.content.startsWith("/chat-register")){
+        let data = "/chat-register"
+        let githubId = message.content.slice(data.length,message.content.length).trim()
+        userId[message.author.id] = githubId
+    }
 })
+
+cron.schedule("30 19 * * *", async() => {
+    for(let key in userId){
+        let preVal = await getCount(userId[key])
+        if (preVal.total_count<1){
+            let user = await client.users.fetch(key)
+            user.send("You have done zero contribution today in github")
+        }
+    }
+});   
